3fa31d9c6d62e25add95c4a6080bdd64c2d73c42
mahesfsp <mahesinfotech2012@gmail.com>
2024-09-15 08:00:39 +0400

    frontend setup

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,29 @@
+import { Fragment, useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+
+export default function Home(){
+
+    const [products,setProducts] = useState([]);
+    useEffect(()=>{
+        fetch(process.env.REACT_APP_API_URL+'/products')
+        .then((res)=>res.json())
+        .then(res=>setProducts(res))
+
+    },[])
+
+
+    return <Fragment>
+      
+    <h1 id="products_heading">Latest Products</h1>
+
+    <section id="products" className="container mt-5">
+      <div className="row">
+      <ProductCard/>
+
+      </div>
+    </section>
+
+  
+
+    </Fragment>
+}
